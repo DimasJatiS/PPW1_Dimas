51f91c75a5af16be3d448a49bc0077a2ae3bb0ee
Dimas Jati Satria <jati.satria000@gmail.com>
2025-03-21 23:01:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglePertemuan(id) {
+    const content = document.getElementById(id);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }
+  
